f68a0ba2b487b207aa12d33de737d4bd9c7b0d05
frank-ec <franklynlara@hotmail.com>
2024-02-06 15:12:46 -0500

    camnia enlaces

--- seguros.php
+++ seguros.php
@@ -16,18 +16,18 @@
 						<a href="https://ias.issfa.mil.ec/consultaRPIS/Consulta.jsf" target="_blank"><img align="center" border="3" height="50" width="100" src="img/issfa.png"><span> Consulta de calificación de atenciones - ISSFA.</span></a>
 						<br><br>
 
-						<a href="https://www.isspol.org.ec:2306/certificados-de-afiliacion" target="_blank"><img align="center" border="3" height="50" width="100" src="img/isspol.png"><span> Consulta de calificación de atenciones -  ISSPOL</span></a>
+						<a href="https://siisspolweb.isspol.org.ec/?_gl=1*1tlnkaf*_ga*Mzk2Nzc3MTY0LjE3MDcyNDk2MDM.*_ga_LY334390X5*MTcwNzI0OTYwMy4xLjEuMTcwNzI0OTc4Ni4wLjAuMA..#/publico/reporteAfiliacionExterno" target="_blank"><img align="center" border="3" height="50" width="100" src="img/isspol.png"><span> Consulta de calificación de atenciones -  ISSPOL</span></a>
 						<br><br>
 								
 
-						<a href="https://coresalud.msp.gob.ec/coresalud/app.php/publico/rpis/afiliacion/consulta" target="_blank"><img align="center" border="3" height="50" width="100" src="img/rpis.png"><span> Consulta de calificación de atenciones - R.P.I.S.</span></a>
+						<a href="https://coberturasalud.msp.gob.ec/" target="_blank"><img align="center" border="3" height="50" width="100" src="img/rpis.png"><span> Consulta de calificación de atenciones - R.P.I.S.</span></a>
 								 
 						<br><br>
 
-						<a href="http://www.calidadsalud.gob.ec/acess-app-servicio-ciudadano/public/medicinaPrepagada/consulta.jsf" target="_blank"><img align="center" border="3" height="50" width="100" src="http://www.elciudadano.gob.ec/wp-content/uploads/2017/01/C28oVlBWgAAkN8w-680x320.jpg"><span> Beneficiarios de Seguros Privados - A.C.C.E.S.S.</span></a>
+						<a href="#" target="_blank"><img align="center" border="3" height="50" width="100" src="img/guardar.png"><span> Beneficiarios de Seguros Privados - A.C.C.E.S.S.</span></a>
 						<br><br>
 
-						<a href="https://servicios.registrocivil.gob.ec/cdd/" target="_blank"><img align="center" border="3" height="50" width="100" src="http://www.telecomunicaciones.gob.ec/wp-content/uploads/2014/04/30-04-2014-Registro-c.png"><span>Consulta de Datos de Identificion - Registro Civil</span></a>
+						<a href="#" target="_blank"><img align="center" border="3" height="50" width="100" src="http://www.telecomunicaciones.gob.ec/wp-content/uploads/2014/04/30-04-2014-Registro-c.png"><span>Consulta de Datos de Identificion - Registro Civil</span></a>
 						<br><br>
 
 					</center>
